a7543ae764ff2fdb2bae98f038e652de30cd0a5e
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-28 21:52:52 +0000

    update() and shell.stop()

--- DIVI/src/compiler/compile.py
+++ DIVI/src/compiler/compile.py
@@ -1,7 +1,7 @@
 import os
 
 TOKENS = [
-	"shell.log('", "')", "update()"
+	"shell.log('", "')", "update()", "shell.stop()"
 ]
 
 def write(INPUT):
@@ -10,5 +10,7 @@ def write(INPUT):
 			if i == TOKENS[0]:
 				memory = INPUT.split("'")
 				print(memory[1])
-			if i == TOKENS[1]:
+			elif i == TOKENS[2]:
 				os.system('git clone https://github.com/MortyHub/DIVI-Interprater.git')
+			elif i == TOKENS[3]:
+				exit()
