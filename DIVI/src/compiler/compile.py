ca58505f0445c2961904a2d219655297ff1e3dc2
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-28 22:13:26 +0000

    stuff in update

--- DIVI/src/compiler/compile.py
+++ DIVI/src/compiler/compile.py
@@ -11,6 +11,7 @@ def write(INPUT):
 				memory = INPUT.split("'")
 				print(memory[1])
 			elif i == TOKENS[2]:
+				print('Will only work with git installed:\nWindows: https://gitforwindows.org\nMac: $ brew install git in Command Line\nLinux: sudo apt-get install git In Shell')
 				os.system('git clone https://github.com/MortyHub/DIVI-Interpreter')
 			elif i == TOKENS[3]:
 				exit()
