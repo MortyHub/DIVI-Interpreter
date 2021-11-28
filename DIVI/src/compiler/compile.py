df3a0be099d67e99b90e0dfedf06f1c5bf315cb0
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-28 21:44:46 +0000

    update() command

--- DIVI/src/compiler/compile.py
+++ DIVI/src/compiler/compile.py
@@ -1,5 +1,7 @@
+import os
+
 TOKENS = [
-	"shell.log('", "')"
+	"shell.log('", "')", "update()"
 ]
 
 def write(INPUT):
@@ -8,3 +10,5 @@ def write(INPUT):
 			if i == TOKENS[0]:
 				memory = INPUT.split("'")
 				print(memory[1])
+			if i == TOKENS[1]:
+				os.system('git clone https://github.com/MortyHub/DIVI-Interprater.git')
