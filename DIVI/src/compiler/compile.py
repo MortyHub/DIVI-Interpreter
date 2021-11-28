68f510b9e6437e540282b52cb5ae6ae38aa1e206
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-28 23:22:58 +0000

    added multiline()

--- DIVI/src/compiler/compile.py
+++ DIVI/src/compiler/compile.py
@@ -1,8 +1,14 @@
 import os
 
 TOKENS = [
-	"shell.log('", "')", "update()", "shell.stop()"
+	"shell.log('", "')", "update()", "shell.stop()", "multiline()"
 ]
+MULTI = []
+
+def Mult(inc):
+	for i in range(len(inc)):
+		write(inc[i])
+
 
 def write(INPUT):
 	for i in TOKENS:
@@ -15,3 +21,24 @@ def write(INPUT):
 				os.system('git clone https://github.com/MortyHub/DIVI-Interpreter')
 			elif i == TOKENS[3]:
 				exit()
+			elif i == TOKENS[4]:
+				print("Type: end, to end the the script")
+				line = 1
+				y = True
+				while y == True:
+					inpu = input(str(line) + ' >> ')
+					line += 1
+					if inpu == 'end':
+						y = False
+						Mult(MULTI)
+					else:
+						MULTI.append(inpu)
+						
+				
+
+
+def error(er):
+	print(colored(235, 94, 94, 'Error: ' + er))
+
+def colored(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
