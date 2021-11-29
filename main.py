6fcf893e8db43c2cb43b09891060e785723acf32
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-11-29 02:47:43 +0000

    momp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from DIVI.src.compiler.compile import write
 
-print('DIVI 0.0.3 (default, Nov 28 2021, 15:34:10)\n')
+
+print('Divi 0.0.3 (default, Nov 28 2021, 15:34:10)')
 while True:
-	inp = input("DIVI >> ")
+	inp = input("Divi >> ")
 	write(inp)
