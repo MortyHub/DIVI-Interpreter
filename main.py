d4065ab3a9010b7c6856896d0cc554d21d3f394f
MortyHub <83893877+MortyHub@users.noreply.github.com>
2021-12-05 15:49:49 -0600

    Ready For Stable Release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DIVI.src.compiler.compile import write
-print('Divi 0.0.3 (default, Nov 28 2021, 15:34:10)')
+print('Divi 1.0.0 (default, Nov 28 2021, 15:34:10)')
 
 while True:
 	inp = input("Divi >> ")
