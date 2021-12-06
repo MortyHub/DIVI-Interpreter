379e59281f986428c9d10328c005c7364d502a1e
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-12-06 04:54:34 +0000

    new release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DIVI.src.compiler.compile import write
-print('Divi 1.0.0 (default, Nov 28 2021, 15:34:10)')
+print('Divi v1.0.1 (default, Nov 28 2021, 15:34:10)')
 
 while True:
 	inp = input("Divi >> ")
