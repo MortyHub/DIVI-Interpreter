0d6a33153316b863cfbbffee70124c9c64785f3b
Sarthing Gamer <83893877+MortyHub@users.noreply.github.com>
2021-12-22 07:09:02 +0000

    Fully Finished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DIVI.src.compiler.compile import write
-print('Divi v1.5.0 (default, Nov 28 2021, 15:34:10)')
+print('Divi Finished (default, Nov 28 2021, 15:34:10)')
 
 while True:
 	inp = input("Divi >> ")
